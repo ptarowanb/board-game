6ae622785fef393dc146bf8b72325cac157b5ee9
develon3 <131450386+ptarowanb@users.noreply.github.com>
2025-03-18 10:49:48 +0900

    새로 추가

---
+++ badugi/Assets/Scripts/android/m_logo/csLogo.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class csLogo : MonoBehaviour {
+
+    bool isAuto;
+    private void Awake()
+    {
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
+        Screen.SetResolution(csFlagDef.SCREEN_WIDTH, csFlagDef.SCREEN_HEIGHT, true);
+
+        isAuto = false;
+#if UNITY_ANDROID && !UNITY_EDITOR
+        string arguments = "";
+
+        AndroidJavaClass UnityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        AndroidJavaObject currentActivity = UnityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+
+        AndroidJavaObject intent = currentActivity.Call<AndroidJavaObject>("getIntent");
+        bool hasExtra = intent.Call<bool>("hasExtra", "arguments");
+
+        if (hasExtra)
+        {
+            AndroidJavaObject extras = intent.Call<AndroidJavaObject>("getExtras");
+            arguments = extras.Call<string>("getString", "arguments");
+
+            if (string.IsNullOrEmpty(arguments) == false)
+            {
+                string[] split = arguments.Split('\n');
+
+                if (split.Count() > 0)
+                {
+                    string command = split.First();
+
+                    if (command == "auto" && split.Count() == 3)
+                    {
+                        isAuto = true;
+                    }
+                }
+            }
+        }
+#endif
+    }
+    // Use this for initialization
+    void Start ()
+    {
+        if (isAuto)
+        {
+            SceneManager.LoadScene("M_Android");
+        }
+        else
+        {
+            StartCoroutine("CoNextScene");
+        }
+	}
+    IEnumerator CoNextScene()
+    {
+        yield return new WaitForSeconds(3.0f);
+        SceneManager.LoadScene("M_Android");
+    }
+	
+}
